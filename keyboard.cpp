88da9337a153abd8e3e61cd2c7b98b26728265f3
Rickard Hallerbäck <rickard.hallerback@gmail.com>
2023-09-03 23:51:17 +0200

    Applying effects to wave

--- keyboard.cpp
+++ keyboard.cpp
@@ -63,6 +63,38 @@ void Keyboard::printInstructions() {
   printw("\n\n");
 }
 
+std::vector<short> convertToVector(const char *data, int numSamples) {
+  // Cast the char pointer to a short pointer.
+  const short *shortData = reinterpret_cast<const short *>(data);
+  return std::vector<short>(shortData, shortData + numSamples);
+}
+
+void splitChannels(const char *data, size_t dataSize, std::vector<short> &left,
+                   std::vector<short> &right) {
+  // Ensure the data is a multiple of 4 bytes (2 channels * 2 bytes per sample)
+  if (dataSize % 4 != 0) {
+    std::cerr << "Invalid data size!" << std::endl;
+    return;
+  }
+
+  size_t numStereoPairs = dataSize / 4; // Total number of left-right pairs
+  size_t numSamplesPerChannel =
+      dataSize / 2 / 2; // Number of samples for each channel
+
+  left.resize(numSamplesPerChannel);
+  right.resize(numSamplesPerChannel);
+
+  for (size_t i = 0, j = 0; i < dataSize; i += 4, j++) {
+
+    left[j] =
+        static_cast<short>((static_cast<unsigned char>(data[i + 1]) << 8) |
+                           static_cast<unsigned char>(data[i]));
+    right[j] =
+        static_cast<short>((static_cast<unsigned char>(data[i + 3]) << 8) |
+                           static_cast<unsigned char>(data[i + 2]));
+  }
+}
+
 void Keyboard::prepareSound(int sampleRate, ADSR &adsr, Sound::WaveForm f,
                             Effects &effects) {
   int length = adsr.getLength();
@@ -98,11 +130,37 @@ void Keyboard::prepareSound(int sampleRate, ADSR &adsr, Sound::WaveForm f,
             format = AL_FORMAT_STEREO16;
           }
         }
-        printf("channels: %d, sampleRate: %d, bps: %d, data size: %d\n",
-               channels, sampleRate, bps, size);
 
-        alBufferData(this->buffers[bufferIndex], format, data, size,
-                     sampleRate);
+        if (channels == 2) {
+          std::vector<short> buffer_left;
+          std::vector<short> buffer_right;
+
+          splitChannels(data, size, buffer_left, buffer_right);
+
+          std::vector<short> buffer_left_effect = effects.apply(buffer_left);
+          std::vector<short> buffer_right_effect = effects.apply(buffer_right);
+
+          std::vector<short> interleaved;
+          interleaved.reserve(buffer_left_effect.size() *
+                              2); // Reserve space to optimize performance
+
+          for (size_t i = 0; i < buffer_left_effect.size(); ++i) {
+            interleaved.push_back(buffer_left_effect[i]);
+            interleaved.push_back(buffer_right_effect[i]);
+          }
+
+          alBufferData(this->buffers[bufferIndex], format, interleaved.data(),
+                       size, sampleRate);
+
+        } else {
+
+          std::vector<short> buffer_mono = convertToVector(data, size);
+          std::vector<short> buffer = effects.apply(buffer_mono);
+
+          alBufferData(this->buffers[bufferIndex], format, buffer.data(), size,
+                       sampleRate);
+        }
+
       } else {
         fprintf(stderr, "Error loading file: %s\n",
                 this->soundMap[key].c_str());
