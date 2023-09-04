586b169b741aee33fddeda03da880317ccfc17a1
Rickard Hallerbäck <rickard.hallerback@gmail.com>
2023-09-04 20:40:03 +0200

    updating the download script

--- media/download.sh
+++ media/download.sh
@@ -4,13 +4,13 @@
 BASE_URL="https://theremin.music.uiowa.edu/sound%20files/MIS/Piano_Other/piano"
 
 # Notes array
-NOTES=(C C# D D# E F F# G G# A A# B)
+NOTES=(A Bb B C Dd D E F Gb G Ab) 
 
 # Loop through octaves from 0 to 8
 for OCTAVE in {0..8}; do
     for NOTE in "${NOTES[@]}"; do
         # Convert '#' to 'b' naming convention
-        FILE_NOTE=$(echo "$NOTE" | sed 's/#/b/')
+        FILE_NOTE=$(echo "$NOTE")
 
         # Construct the URL
         URL="$BASE_URL/Piano.pp.$FILE_NOTE$OCTAVE.aiff"
@@ -28,3 +28,12 @@ done
 
 echo "Download completed."
 
+# Convert aiff to wave, raise volume 25 db
+for file in *.aiff; do                            
+    ffmpeg -i "$file" "${file%.aiff}.wav"                     
+done
+for file in Piano.pp.*; do                      
+    ffmpeg -y -i "$file" -filter:a "volume=25dB" "louder_$file"
+done
+for f in louder_*; do mv "$f" "${f#louder_}"; done
+rm *.aiff
